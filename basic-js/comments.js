343dd562b255c9c77351b67cf4ca0b91f19d759d
Fernando Alves <fernando.luis.alves@outlook.com>
2022-11-15 21:07:01 -0300

    first commit

---
+++ basic-js/comments.js
@@ -0,0 +1,13 @@
+//variables
+const myName = 'Fernando';
+const date = new Date();
+
+const template = `My name is ${myName}. I'm learning JavaScript at ${date}`;
+
+//run commands
+console.log(template);
+
+/*
+command1
+command2
+*/
---
+++ basic-js/concat-strings.js
@@ -0,0 +1,6 @@
+const myName = 'Fernando';
+const date = new Date();
+
+const template = `My name is ${myName}. I'm learning JavaScript at ${date}`;
+
+console.log(template);
